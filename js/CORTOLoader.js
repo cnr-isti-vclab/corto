dfb37e008631270f774d15c12e77b16d14a3a2fb
Federico Ponchio <ponchio@gmail.com>
2017-05-03 11:47:51 +0200

    Added license to LGPL3

--- js/CORTOLoader.js
+++ js/CORTOLoader.js
@@ -1,3 +1,20 @@
+/*
+Corto
+
+Copyright(C) 2017 - Federico Ponchio
+ISTI - Italian National Research Council - Visual Computing Lab
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  You should have received 
+a copy of the GNU General Public License along with Corto.
+If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 THREE.CORTOLoader = function(options, manager) {

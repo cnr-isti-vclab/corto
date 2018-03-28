7aa667050eb09274fd99feacede07f6299a6acbf
Marco Potenziani <marco.potenziani@isti.cnr.it>
2018-03-28 12:52:23 +0200

    copyright update
    js files

--- js/attribute.js
+++ js/attribute.js
@@ -1,23 +1,21 @@
 /*
 Corto
+Copyright (c) 2017-2018, Visual Computing Lab, ISTI - CNR
+All rights reserved.
 
-Copyright(C) 2017 - Federico Ponchio
-ISTI - Italian National Research Council - Visual Computing Lab
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  You should have received 
-a copy of the GNU General Public License along with Corto.
-If not, see <http://www.gnu.org/licenses/>.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-
-
-
 
 function Attribute(name, q, components, type, strategy) {
 	var t = this;

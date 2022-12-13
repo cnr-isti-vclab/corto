60c8af20e411ac362beb31779d84b17b1344f848
Nicola <nicpal2000@gmail.com>
2022-12-13 12:03:30 +0100

    Completed benchmark program

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,15 @@ a copy of the GNU General Public License along with Corto.
 If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*
-	TO TEST
-	- Tunstall / Zstd
-	- N tests per model, print the average encode / decode score
-	- With / without acceleration
-	- With different compression levels
-	- Different frame sizes?
+/* CRASH
+*		name		idx		Opt?	NonOpt?		TUNSTALL?	ZSTD?
+	
+	-	gargo		8
+	-	laurana		9
+	-	mba			10		YES					YES			YES
+	-	moebius		11		/		YES			YES			/
+	-	sphere		17		/		YES			YES			YES
+
 */
 
 #include <assert.h>
@@ -39,6 +41,8 @@ If not, see <http://www.gnu.org/licenses/>.
 #include "meshloader.h"
 #include "timer.h"
 
+#define ENABLE_CONSOLE_LOG	1
+
 
 using namespace crt;
 using namespace std;
@@ -47,7 +51,6 @@ using namespace tinyply;
 struct MeshData
 {
 	uint32_t ColorComponents;
-
 };
 
 struct CompressionSettings
@@ -74,11 +77,12 @@ struct CompressionSettings
 struct CompressionResult
 {
 	MeshData Mesh;
-	Encoder CompressionEncoder;
+	Encoder* CompressionEncoder;
 
 	uint32_t NVerts;
 	uint32_t NFaces;
 	uint32_t EncodedSize;
+	uint32_t UnencodedSize;
 	
 	float Ratio;
 	float HeaderSizeBpv;
@@ -104,6 +108,12 @@ struct DecompressionResult
 
 static bool endsWith(const std::string& str, const std::string& suffix) {
 	return str.size() >= suffix.size() && !str.compare(str.size()-suffix.size(), suffix.size(), suffix);
+}
+
+static ifstream::pos_type filesize(const char* filename)
+{
+	ifstream in(filename, ifstream::ate | ifstream::binary);
+	return in.tellg();
 }
 
 static vector<string> GetModelPaths(string root)
@@ -166,58 +176,60 @@ CompressionResult CompressModel(string path, CompressionSettings& settings)
 	crt::Timer timer;
 
 	// Encode
-	crt::Encoder encoder(loader.nvert, loader.nface, crt::Stream::ZSTD);
+	crt::Encoder* encoder = new Encoder(loader.nvert, loader.nface, settings.Entropy);
+	ret.CompressionEncoder = encoder;
 
-	encoder.exif = loader.exif;
+	encoder->setOptimizationMode(settings.Optimize);
+	encoder->setZstdCompressionLevel(settings.CompressionLevel);
+	encoder->exif = loader.exif;
 	//add and override exif properties
 	for (auto it : settings.Exif)
-		encoder.exif[it.first] = it.second;
+		encoder->exif[it.first] = it.second;
 
 	for (auto& g : loader.groups)
-		encoder.addGroup(g.end, g.properties);
+		encoder->addGroup(g.end, g.properties);
 
 	if (settings.PointCloud) {
 		if (settings.VertexBits)
-			encoder.addPositionsBits(loader.coords.data(), settings.VertexBits);
+			encoder->addPositionsBits(loader.coords.data(), settings.VertexBits);
 		else
-			encoder.addPositions(loader.coords.data(), settings.VertexQ);
+			encoder->addPositions(loader.coords.data(), settings.VertexQ);
 
 	}
 	else {
 		if (settings.VertexBits)
-			encoder.addPositionsBits(loader.coords.data(), loader.index.data(), settings.VertexBits);
+			encoder->addPositionsBits(loader.coords.data(), loader.index.data(), settings.VertexBits);
 		else
-			encoder.addPositions(loader.coords.data(), loader.index.data(), settings.VertexQ);
+			encoder->addPositions(loader.coords.data(), loader.index.data(), settings.VertexQ);
 	}
 	//TODO add suppor for wedge and face attributes adding simplex attribute
 	if (loader.norms.size() && settings.NormBits > 0)
-		encoder.addNormals(loader.norms.data(), settings.NormBits, prediction);
+		encoder->addNormals(loader.norms.data(), settings.NormBits, prediction);
 
 	if (loader.colors.size() && settings.RBits > 0) {
 		if (loader.nColorsComponents == 3) {
-			encoder.addColors3(loader.colors.data(), settings.RBits, settings.GBits, settings.BBits);
+			encoder->addColors3(loader.colors.data(), settings.RBits, settings.GBits, settings.BBits);
 		}
 		else
-			encoder.addColors(loader.colors.data(), settings.RBits, settings.GBits, settings.BBits, settings.ABits);
+			encoder->addColors(loader.colors.data(), settings.RBits, settings.GBits, settings.BBits, settings.ABits);
 	}
 
 	if (loader.uvs.size() && settings.UVBits > 0)
-		encoder.addUvs(loader.uvs.data(), pow(2, -settings.UVBits));
+		encoder->addUvs(loader.uvs.data(), pow(2, -settings.UVBits));
 
 	if (loader.radiuses.size())
-		encoder.addAttribute("radius", (char*)loader.radiuses.data(), crt::VertexAttribute::FLOAT, 1, 1.0f);
-	encoder.encode();
+		encoder->addAttribute("radius", (char*)loader.radiuses.data(), crt::VertexAttribute::FLOAT, 1, 1.0f);
+	encoder->encode();
 
 	// Setup result
-	ret.CompressionEncoder = encoder;
 	ret.EncodingTime = timer.elapsed();
 	ret.Error = 0;
-	ret.NVerts = encoder.nvert;
-	ret.NFaces = encoder.nface;
-	ret.EncodedSize = encoder.stream.size();
-	ret.HeaderSizeBpv = (float)encoder.header_size / ret.NVerts;
-	ret.Bpv = 8.0f * encoder.stream.size() / ret.NVerts;
-	ret.Ratio = 100.0f * encoder.stream.size() / (ret.NVerts * 12 + ret.NFaces * 12);
+	ret.NVerts = encoder->nvert;
+	ret.NFaces = encoder->nface;
+	ret.EncodedSize = encoder->stream.size();
+	ret.HeaderSizeBpv = (float)encoder->header_size / ret.NVerts;
+	ret.Bpv = 8.0f * encoder->stream.size() / ret.NVerts;
+	ret.Ratio = 100.0f * encoder->stream.size() / (ret.NVerts * 12 + ret.NFaces * 12);
 
 	return ret;
 }
@@ -229,13 +241,13 @@ DecompressionResult DecompressModel(string path, CompressionResult& res)
 	timer.start();
 
 	// Decode
-	crt::Decoder decoder(res.CompressionEncoder.stream.size(), res.CompressionEncoder.stream.data());
+	crt::Decoder decoder(res.CompressionEncoder->stream.size(), res.CompressionEncoder->stream.data());
 	assert(decoder.nface == res.NFaces);
 	assert(decoder.nvert == res.NVerts);
 
 	crt::MeshLoader out;
-	out.nvert = res.CompressionEncoder.nvert;
-	out.nface = res.CompressionEncoder.nface;
+	out.nvert = res.CompressionEncoder->nvert;
+	out.nface = res.CompressionEncoder->nface;
 	out.coords.resize(res.NVerts * 3);
 	decoder.setPositions(out.coords.data());
 	if (decoder.data.count("normal")) {
@@ -259,7 +271,9 @@ DecompressionResult DecompressModel(string path, CompressionResult& res)
 		out.index.resize(res.NFaces * 3);
 		decoder.setIndex(out.index.data());
 	}
+	cout << endl << "Data set";
 	decoder.decode();
+	cout << endl;
 
 	int delta = timer.elapsed();
 	float DecodingTime;
@@ -270,69 +284,115 @@ DecompressionResult DecompressModel(string path, CompressionResult& res)
 	if (!endsWith(path, ".crt"))
 		path += ".crt";
 
-	FILE* file = fopen(("cortomodels/compressed/" + path).c_str(), "wb");
+	/*FILE* file = fopen(("cortomodels/compressed/" + path).c_str(), "wb");
 	if (!file) {
 		cerr << "Could not open file: " << path << endl;
 		return 1;
 	}
-	size_t count = res.CompressionEncoder.stream.size();
-	size_t written = fwrite(res.CompressionEncoder.stream.data(), 1, count, file);
+	size_t count = res.CompressionEncoder->stream.size();
+	size_t written = fwrite(res.CompressionEncoder->stream.data(), 1, count, file);
 	if (written != count) {
 		cerr << "Failed saving file: " << "test.crt" << endl;
 		return 1;
 	}
 	std::vector<std::string> comments;
 	if (!path.empty())
-		out.savePly("cortomodels/decompressed/" + path, comments);
+		out.savePly("cortomodels/decompressed/" + path + ".ply", comments);
+		*/
 
 	return ret;
 }
 
-void ResultMessage(crt::Stream::Entropy encodingType, bool accelerated, int compressionLevel, string modelPath,
-	float encoding, float decoding)
+void ResultMessage(CompressionSettings& settings, string modelPath, CompressionResult cr, DecompressionResult dr, ofstream& outFile)
 {
-	string encodingStirng = encodingType == crt::Stream::Entropy::TUNSTALL ? "Tunstall" : "ZSTD";
-	string acceleratedString = accelerated ? "Accelerated" : "Not accelerated";
+	string encodingString = settings.Entropy == crt::Stream::Entropy::TUNSTALL ? "Tunstall" : "ZSTD";
+	string acceleratedString = settings.Optimize ? "Accelerated" : "Unaccelerated";
 
+	outFile << "MODEL: " << modelPath << "\t\t" << acceleratedString << " " << encodingString << " level " << settings.CompressionLevel << endl;
+	outFile << "COMPRESSSION DATA:" << endl;
+	outFile << "Uncompressed size:\t" << cr.UnencodedSize<< "\tEncoded size: " << cr.EncodedSize << endl;
+	outFile << "Encoding time:\t" << cr.EncodingTime << "\tDecoding time: " << dr.DecodingTime<< endl;
+	outFile << "Vertices:\t" << cr.NVerts << "\tCompression ratio: " << cr.Ratio << endl << endl;
 }
 
 int main(int argc, char *argv[]) {
 	string input;
 	string output;
 	string plyfile;
+	ofstream results("results.txt");
 
-	int nIterations = 10;
-	int minCompressionLevel = -7, maxCompressionLevel = 22, compressionLevelIncrease = 3;
+	int nIterations = 1, modelIncrease = 1;
+	int minCompressionLevel = -5, maxCompressionLevel = 22, compressionLevelIncrease = 5;
 	bool accelerateCompression[2] = { false, true };
+	bool hasCompressionLevels[2] = { false, true };
 	std::vector<std::string> modelPaths = GetModelPaths("cortomodels/models");
 	crt::Stream::Entropy toCompare[2] = {crt::Stream::TUNSTALL, crt::Stream::ZSTD};
 
 	// Entropy method
 	for (uint32_t e = 0; e < 2; e++)
 	{
+#if ENABLE_CONSOLE_LOG
+		cout << "TESTING " << (toCompare[e] == crt::Stream::TUNSTALL ? "TUNSTALL" : "ZSTD") << endl;
+#endif
 		// Compression acceleration
 		for (uint32_t a = 0; a < 2; a++)
 		{
-			// Compression levels
-			for (uint32_t l = minCompressionLevel; l < maxCompressionLevel; l += compressionLevelIncrease)
+#if ENABLE_CONSOLE_LOG
+			cout << "Acceleration: " << accelerateCompression[a] << endl;
+#endif
+			for (uint32_t m = 0; m <modelPaths.size(); m += modelIncrease)
 			{
-				float encodingTime = 0, decodingTime = 0;
-				for (uint32_t m = 0; m < modelPaths.size(); m++)
+#if ENABLE_CONSOLE_LOG
+				cout << "Model: " << modelPaths[m] << endl;
+#endif
+				int minLevel = 0, maxLevel = 1;
+				if (hasCompressionLevels[e])
 				{
+					minLevel = minCompressionLevel;
+					maxLevel = maxCompressionLevel;
+				}
+				// Compression levels
+				for (int l = minLevel; l < maxLevel; l += compressionLevelIncrease)
+				{
+#if ENABLE_CONSOLE_LOG
+					cout << "Compression level: " << l << endl;
+#endif
+					float encodingTime = 0, decodingTime = 0;
+					CompressionSettings settings;
+
+					settings.CompressionLevel = l;
+					settings.Optimize = accelerateCompression[a];
+					settings.Entropy = toCompare[e];
+
+					CompressionResult compResult;
+					DecompressionResult decompResult;
+
 					for (uint32_t i = 0; i < nIterations; i++)
 					{
-						CompressionResult compResult = CompressModel(modelPaths[m], CompressionSettings()).EncodingTime;
-						DecompressionResult decompResult = DecompressModel(modelPaths[m], compResult);
+						cout << "Compressing...";
+						compResult = CompressModel("cortomodels/models/" + modelPaths[m], settings);
+						cout << "compressed." << endl;
+
+						cout << "Decompressing...";
+						decompResult = DecompressModel(modelPaths[m], compResult);
+						cout << "decompressed." << endl;
 
 						encodingTime += compResult.EncodingTime;
-						decodingTime += decompResult.DecodingTime;
+						decodingTime += decompResult.DecodingTime;						
+
+						delete compResult.CompressionEncoder;
 					}
 
-					ResultMessage(toCompare[e], a, l, modelPaths[m], encodingTime / nIterations, decodingTime / nIterations);
+					compResult.UnencodedSize = filesize(("cortomodels/models/" + modelPaths[m]).c_str());
+					compResult.EncodingTime = (float)encodingTime / nIterations;
+					decompResult.DecodingTime = (float)decodingTime / nIterations;
+
+					ResultMessage(settings, modelPaths[m], compResult, decompResult, results);
 				}
 			}
 		}
 	}
 
+	results.close();
 	return 0;
 }
